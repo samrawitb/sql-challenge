333fa77d61054490deb0d6f41482c9bb20105255
Samrawit B <samrawitbas@gmail.com>
2024-06-21 14:07:16 -0400

    submit sql challenge

--- EmployeeSQL/SQL_challange_postgres.sql
+++ EmployeeSQL/SQL_challange_postgres.sql
@@ -46,7 +46,8 @@ CREATE TABLE "titles" (
 -- 1. List the employee number, last name, frist name, sex and salary of each employee
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
-JOIN salaries s ON e.emp_no = s.emp_no;
+JOIN salaries s ON e.emp_no = s.emp_no
+ORDER BY e.last_name ASC;
 
 -- 2. List first name, last name, and hire date for the employees who were hired in 1986.
 SELECT e.last_name, e.first_name, e.hire_date
